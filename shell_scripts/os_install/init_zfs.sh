b9f39921d22464eea04b1e657f1e74b1f269ca43
jon Rurka <jon.rurka0@gmail.com>
2023-06-28 22:54:53 -0700

    Create init_zfs.sh

---
+++ shell_scripts/os_install/init_zfs.sh
@@ -0,0 +1,11 @@
+apt install --yes debootstrap gdisk zfsutils-linux
+
+systemctl stop zed
+
+DISK0=/dev/disk/by-id/nvme-WDC_CL_SN720_SDAQNTW-512G-2000_21060X802821
+DISK1=/dev/disk/by-id/nvme-WDC_CL_SN720_SDAQNTW-512G-2000_21060X801103
+
+swapoff --all
+
+sgdisk --zap-all $DISK0
+sgdisk --zap-all $DISK1
