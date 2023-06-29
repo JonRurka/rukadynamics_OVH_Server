cd23777e145f1b179e4091bcd68c72de14dd5ab0
jon Rurka <jon.rurka0@gmail.com>
2023-06-29 12:06:45 -0700

    Update create_zfs_datasets.sh

--- shell_scripts/os_install/create_zfs_datasets.sh
+++ shell_scripts/os_install/create_zfs_datasets.sh
@@ -4,7 +4,7 @@ set -x #echo on
 zfs create -o canmount=off -o mountpoint=none rpool/ROOT
 zfs create -o canmount=off -o mountpoint=none bpool/BOOT
 
-export UUID=$(dd if=/dev/urandom bs=1 count=100 2>/dev/null | tr -dc 'a-z0-9' | cut -c-6)
+#export UUID=$(dd if=/dev/urandom bs=1 count=100 2>/dev/null | tr -dc 'a-z0-9' | cut -c-6)
 
 zfs create -o mountpoint=/ \
     -o com.ubuntu.zsys:bootfs=yes \
