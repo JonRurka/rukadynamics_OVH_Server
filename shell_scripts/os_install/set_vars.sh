39c4ce57d3fb51a820d55ce99d4c0c585a29056d
jon Rurka <jon.rurka0@gmail.com>
2023-06-29 11:25:47 -0700

    Update set_vars.sh

--- shell_scripts/os_install/set_vars.sh
+++ shell_scripts/os_install/set_vars.sh
@@ -1,8 +1,8 @@
-export DISK0=/
+export DISK0=/dev/disk/by-id/nvme-WDC_CL_SN720_SDAQNTW-512G-2000_21060X802821
 set
 
-export DISK1=/
+export DISK1=/dev/disk/by-id/nvme-WDC_CL_SN720_SDAQNTW-512G-2000_21060X801103
 set
 
-export UUID=
+export UUID=asdkpm
 set
