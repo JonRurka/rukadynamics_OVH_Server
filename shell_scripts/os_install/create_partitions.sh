48617dbd2c9402ddf3bd26d457864b7726d97b60
jon Rurka <jon.rurka0@gmail.com>
2023-06-28 22:55:40 -0700

    Create create_partitions.sh

---
+++ shell_scripts/os_install/create_partitions.sh
@@ -0,0 +1,15 @@
+# bootloader partitions
+sgdisk     -n1:1M:+512M   -t1:EF00 $DISK0
+sgdisk     -n1:1M:+512M   -t1:EF00 $DISK1
+
+# swap partitions:
+sgdisk     -n2:0:+500M    -t2:FD00 $DISK0
+sgdisk     -n2:0:+500M    -t2:FD00 $DISK1
+
+# boot pool partitions:
+sgdisk     -n3:0:+2G      -t3:BE00 $DISK0
+sgdisk     -n3:0:+2G      -t3:BE00 $DISK1
+
+# root pool partitions:
+sgdisk     -n4:0:0        -t4:BF00 $DISK0
+sgdisk     -n4:0:0        -t4:BF00 $DISK1
