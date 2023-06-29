accef37f46ef56a85fb27646d275ca088bda6333
jon Rurka <jon.rurka0@gmail.com>
2023-06-29 12:15:55 -0700

    Create zfs_ubuntu_setup.sh

---
+++ shell_scripts/os_install/zfs_ubuntu_setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -x #echo on
+
+#call init.sh first
+#call zfs_ubuntu_setup.sh from ssh
+
+sudo -i
+
+source ./set_vars.sh
+source ./init_zfs.sh
+source ./create_partitions.sh
+source ./create_zfs_pools.sh
+source ./create_zfs_datasets.sh
+
+debootstrap focal /mnt
+
+source ./do_install.sh
+#source ./config_install.sh
+#source ./grub_install.sh
+
+echo "Install script finished"
