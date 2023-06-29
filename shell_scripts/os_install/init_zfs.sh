62c2c66c8fc7cb3e08b7c9107462be4dd26d5024
jon Rurka <jon.rurka0@gmail.com>
2023-06-28 23:06:11 -0700

    Update init_zfs.sh

--- shell_scripts/os_install/init_zfs.sh
+++ shell_scripts/os_install/init_zfs.sh
@@ -5,6 +5,9 @@ systemctl stop zed
 DISK0=/dev/disk/by-id/nvme-WDC_CL_SN720_SDAQNTW-512G-2000_21060X802821
 DISK1=/dev/disk/by-id/nvme-WDC_CL_SN720_SDAQNTW-512G-2000_21060X801103
 
+export DISK0
+export DISK1
+
 swapoff --all
 
 sgdisk --zap-all $DISK0
