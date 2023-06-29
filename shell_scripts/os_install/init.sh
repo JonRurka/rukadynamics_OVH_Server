a1f247f5fa4ed166849cc8b3c498b28da9702897
jon Rurka <jon.rurka0@gmail.com>
2023-06-28 22:53:31 -0700

    Create init.sh

---
+++ shell_scripts/os_install/init.sh
@@ -0,0 +1,9 @@
+passwd
+
+sudo apt update
+sudo apt install --yes openssh-server vim
+
+sudo systemctl enable ssh
+sudo systemctl start ssh
+
+sudo gsettings set org.gnome.desktop.media-handling automount false
