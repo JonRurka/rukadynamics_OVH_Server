53993c9609fd44737a3be229ba2406afe0c25717
jon Rurka <jon.rurka0@gmail.com>
2023-06-29 00:12:42 -0700

    Update init_zfs.sh

--- shell_scripts/os_install/init_zfs.sh
+++ shell_scripts/os_install/init_zfs.sh
@@ -5,11 +5,8 @@ apt install --yes debootstrap gdisk zfsutils-linux
 
 systemctl stop zed
 
-DISK0=/dev/disk/by-id/nvme-WDC_CL_SN720_SDAQNTW-512G-2000_21060X802821
-DISK1=/dev/disk/by-id/nvme-WDC_CL_SN720_SDAQNTW-512G-2000_21060X801103
-
-export DISK0
-export DISK1
+echo "export DISK0=/dev/disk/by-id/nvme-WDC_CL_SN720_SDAQNTW-512G-2000_21060X802821" > /etc/profile.d/java.sh
+echo "export DISK1=/dev/disk/by-id/nvme-WDC_CL_SN720_SDAQNTW-512G-2000_21060X801103" > /etc/profile.d/java.sh
 
 swapoff --all
 
