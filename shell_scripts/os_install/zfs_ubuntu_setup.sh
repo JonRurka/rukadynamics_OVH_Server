cd8a42a907df5f1fa14b12e525ed21c8a0ce0c8b
jon Rurka <jon.rurka0@gmail.com>
2023-06-29 18:48:50 -0700

    Update zfs_ubuntu_setup.sh

--- shell_scripts/os_install/zfs_ubuntu_setup.sh
+++ shell_scripts/os_install/zfs_ubuntu_setup.sh
@@ -8,16 +8,29 @@ set -x #echo on
 
 #sudo -i
 
+# run on host with root
 source ./set_vars.sh
 source ./init_zfs.sh
 source ./create_partitions.sh
 source ./create_zfs_pools.sh
 source ./create_zfs_datasets.sh
-
-debootstrap focal /mnt
-
 source ./do_install.sh
+
+chroot /mnt /usr/bin/env DISK=$DISK0 UUID=$UUID bash --login
+
+# Run in chroot
 #source ./config_install.sh
+
+# verify grub: grub-probe /boot
+
 #source ./grub_install.sh
+
+# Wait a while and verify with:
+#cat /etc/zfs/zfs-list.cache/bpool
+#cat /etc/zfs/zfs-list.cache/rpool
+
+# if empty run:
+#zfs set canmount=on bpool/BOOT/ubuntu_$UUID
+#zfs set canmount=on rpool/ROOT/ubuntu_$UUID
 
 echo "Install script finished"
