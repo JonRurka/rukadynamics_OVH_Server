9db8b67b4eb52d4d585edb266bc8adfbdf47e46e
jon Rurka <jon.rurka0@gmail.com>
2023-06-29 22:36:39 -0700

    Update create_zfs_pools.sh

--- shell_scripts/os_install/create_zfs_pools.sh
+++ shell_scripts/os_install/create_zfs_pools.sh
@@ -2,33 +2,32 @@
 set -x #echo on
 
 zpool create \
+    -o ashift=12 \
+    -o autotrim=on \
     -o cachefile=/etc/zfs/zpool.cache \
-    -o ashift=12 -o autotrim=on -d \
-    -o feature@async_destroy=enabled \
-    -o feature@bookmarks=enabled \
-    -o feature@embedded_data=enabled \
-    -o feature@empty_bpobj=enabled \
-    -o feature@enabled_txg=enabled \
-    -o feature@extensible_dataset=enabled \
-    -o feature@filesystem_limits=enabled \
-    -o feature@hole_birth=enabled \
-    -o feature@large_blocks=enabled \
-    -o feature@lz4_compress=enabled \
-    -o feature@spacemap_histogram=enabled \
-    -O acltype=posixacl -O canmount=off -O compression=lz4 \
-    -O devices=off -O normalization=formD -O relatime=on -O xattr=sa \
-    -O mountpoint=/boot -f -R /mnt \
+    -o compatibility=grub2 \
+    -o feature@livelist=enabled \
+    -o feature@zpool_checkpoint=enabled \
+    -O devices=off \
+    -O acltype=posixacl -O xattr=sa \
+    -O compression=lz4 \
+    -O normalization=formD \
+    -O relatime=on \
+    -O canmount=off -O mountpoint=/boot -R /mnt \
     bpool mirror \
     ${DISK0}-part3 \
     ${DISK1}-part3
-sleep 5
+sleep 3
 
 zpool create \
-    -o ashift=12 -o autotrim=on \
-    -O acltype=posixacl -O canmount=off -O compression=lz4 \
-    -O dnodesize=auto -O normalization=formD -O relatime=on \
-    -O xattr=sa -O mountpoint=/ -f -R /mnt \
+    -o ashift=12 \
+    -o autotrim=on \
+    -O acltype=posixacl -O xattr=sa -O dnodesize=auto \
+    -O compression=lz4 \
+    -O normalization=formD \
+    -O relatime=on \
+    -O canmount=off -O mountpoint=/ -R /mnt \
     rpool mirror \
     ${DISK0}-part4 \
     ${DISK1}-part4
-sleep 5
+sleep 3
