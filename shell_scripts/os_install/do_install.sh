40c9195dff3ac308b6ca6875d80af9159227d625
jon Rurka <jon.rurka0@gmail.com>
2023-06-29 20:04:08 -0700

    Update do_install.sh

--- shell_scripts/os_install/do_install.sh
+++ shell_scripts/os_install/do_install.sh
@@ -4,12 +4,15 @@ set -x #echo on
 
 # install first:
 debootstrap focal /mnt
+sleep 3
 
 mkdir /mnt/etc/zfs
 cp /etc/zfs/zpool.cache /mnt/etc/zfs/
+sleep 3
 
 hostname rukadynamics
 hostname > /mnt/etc/hostname
+sleep 3
 
 echo "Set host in /mnt/etc/hosts"
 cp hosts /mnt/etc/
@@ -19,7 +22,9 @@ cp 01-netcfg.yaml /mnt/etc/netplan/
 
 echo "Set package sources in /mnt/etc/apt/sources.list"
 cp sources.list /mnt/etc/apt/
+sleep 3
 
 mount --make-private --rbind /dev  /mnt/dev
 mount --make-private --rbind /proc /mnt/proc
 mount --make-private --rbind /sys  /mnt/sys
+sleep 3
