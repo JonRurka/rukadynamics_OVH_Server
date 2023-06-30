63feb288d52d133659f6c3dc7c1aea3e83ec467a
jon Rurka <jon.rurka0@gmail.com>
2023-06-29 22:45:04 -0700

    Update create_zfs_datasets.sh

--- shell_scripts/os_install/create_zfs_datasets.sh
+++ shell_scripts/os_install/create_zfs_datasets.sh
@@ -12,24 +12,13 @@ zfs create -o mountpoint=/ \
 
 zfs create -o mountpoint=/boot bpool/BOOT/ubuntu_$UUID
 
-zfs create -o com.ubuntu.zsys:bootfs=no \
-    rpool/ROOT/ubuntu_$UUID/srv
 zfs create -o com.ubuntu.zsys:bootfs=no -o canmount=off \
     rpool/ROOT/ubuntu_$UUID/usr
-zfs create rpool/ROOT/ubuntu_$UUID/usr/local
 zfs create -o com.ubuntu.zsys:bootfs=no -o canmount=off \
     rpool/ROOT/ubuntu_$UUID/var
-zfs create rpool/ROOT/ubuntu_$UUID/var/games
 zfs create rpool/ROOT/ubuntu_$UUID/var/lib
-zfs create rpool/ROOT/ubuntu_$UUID/var/lib/AccountsService
-zfs create rpool/ROOT/ubuntu_$UUID/var/lib/apt
-zfs create rpool/ROOT/ubuntu_$UUID/var/lib/dpkg
-zfs create rpool/ROOT/ubuntu_$UUID/var/lib/NetworkManager
 zfs create rpool/ROOT/ubuntu_$UUID/var/log
-zfs create rpool/ROOT/ubuntu_$UUID/var/mail
-zfs create rpool/ROOT/ubuntu_$UUID/var/snap
 zfs create rpool/ROOT/ubuntu_$UUID/var/spool
-zfs create rpool/ROOT/ubuntu_$UUID/var/www
 
 zfs create -o canmount=off -o mountpoint=/ \
     rpool/USERDATA
@@ -38,13 +27,35 @@ zfs create -o com.ubuntu.zsys:bootfs-datasets=rpool/ROOT/ubuntu_$UUID \
     rpool/USERDATA/root_$UUID
 chmod 700 /mnt/root
 
+zfs create rpool/ROOT/ubuntu_$UUID/var/cache
+zfs create rpool/ROOT/ubuntu_$UUID/var/lib/nfs
+zfs create rpool/ROOT/ubuntu_$UUID/var/tmp
+chmod 1777 /mnt/var/tmp
+
+zfs create rpool/ROOT/ubuntu_$UUID/var/lib/apt
+zfs create rpool/ROOT/ubuntu_$UUID/var/lib/dpkg
+
+zfs create -o com.ubuntu.zsys:bootfs=no \
+    rpool/ROOT/ubuntu_$UUID/srv
+
+zfs create rpool/ROOT/ubuntu_$UUID/usr/local
+zfs create rpool/ROOT/ubuntu_$UUID/var/games
+zfs create rpool/ROOT/ubuntu_$UUID/var/lib/AccountsService
+zfs create rpool/ROOT/ubuntu_$UUID/var/lib/NetworkManager
+zfs create rpool/ROOT/ubuntu_$UUID/var/lib/docker
+zfs create rpool/ROOT/ubuntu_$UUID/var/mail
+zfs create rpool/ROOT/ubuntu_$UUID/var/snap
+zfs create rpool/ROOT/ubuntu_$UUID/var/www
+
+# mirrored dataset for grub
 zfs create -o com.ubuntu.zsys:bootfs=no bpool/grub
+
+#zfs create -o com.ubuntu.zsys:bootfs=no \
+#    rpool/ROOT/ubuntu_$UUID/tmp
+#chmod 1777 /mnt/tmp
 
 mkdir /mnt/run
 mount -t tmpfs tmpfs /mnt/run
 mkdir /mnt/run/lock
 
-#zfs create -o com.ubuntu.zsys:bootfs=no \
-#    rpool/ROOT/ubuntu_$UUID/tmp
-#chmod 1777 /mnt/tmp
 
